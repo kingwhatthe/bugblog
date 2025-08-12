bbe93c11c3e375dd8a702c87c01cebfb4da735d2
kingwhatthe <sabalschuster06@gmail.com>
2025-08-12 11:15:04 -0400

    quiz page frontend updated

--- quiz.js
+++ quiz.js
@@ -26,16 +26,20 @@ const initQuiz=()=>{
                 continue;
             }
 
-            //Sets image to the one with the above id
-            const myImage = await getQuestionByID(randomNumber);
-            myImage.correct = "none";
-
             //adds id to used ids
             usedIds.add(randomNumber);
+
+            //Sets image to the one with the above id
+            const myImage = await getQuestionByID(randomNumber);
+            if (myImage.in_quiz == 0) {continue;}
+            myImage.correct = "none";
+
+
 
             //adds the image to the array
             questions[i] = myImage; 
         }
+        if (questions.length ==0){console.error("error: no questions found!")};
 
         //initiate progress bar
         const questionProgressContainer = document.querySelector(".progress");
@@ -107,8 +111,40 @@ const initQuiz=()=>{
 
         });
     }
+    const loadPosts = async () =>{
+        //get ranked posts
+        const response = await fetch('./homePagePosts.php');
+        const data = await response.json();
+        const ranked = data.ranked;
 
-    
+        //display ranked posts
+        //get containers
+        const rankedContainer = document.querySelector(".top-ranked");
+        const rankedScrollContainer = rankedContainer.querySelector(".scroll");
+        for(let i = 0; i < ranked.length; i++){
+              const authorLastName = ranked[i].author.substring(ranked[i].author.indexOf(" "),ranked[i].author.length);
+              console.log(authorLastName);
+        
+              let order = getOrderFromTax(ranked[i].taxonomical_info);
+              if ((order+authorLastName).length > 26 ){
+                order = order.substring(0,26-authorLastName.length-3) + "...";
+              } 
+              rankedScrollContainer.innerHTML += 
+                `<div id = "post_1${ranked[i].id}" class = "post">
+                    <img src="${ranked[i].picture_url}" alt="post">
+                    <div class = "overlay-text"><p>${authorLastName}</p><p>${order}</p></div>
+                </div>`;
+        
+        }
+        document.querySelectorAll(".top-ranked .post").forEach((post, index) => {
+            post.addEventListener("click", () => {
+                redirectToPostId(ranked[index].id);
+            });
+        });
+    }
+        
+
+    loadPosts();
     loadQuizInfo();
 
 }
